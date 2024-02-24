11a3c7a2211a23143589458418a752c8da9887d6
DiamondBaka <tomaszmoszczynski18@gmail.com>
2024-02-24 22:44:53 +0000

    home update image

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -2,34 +2,18 @@ import React from 'react';
 import { Dimensions, Image, Text, View } from 'react-native';
 import styles from '../style.js';
 
-const dimensions = Dimensions.get('window');
-const imageHeight = Math.round(dimensions.width * 9 / 16);
-const imageWidth = dimensions.width;
-
-function App() {
-    return (
-        <View>
-        <Image
-                    style={{
-                        width: imageWidth,
-                        height: imageHeight,
-                        resizeMode: 'contain',
-                    }}
-                    source={{
-                        uri: 'https://www.secret-scotland.com/datafiles/uploaded/cmsRefImage/popularPlaces/additional/main/main_77_Duthiebandstand.jpg',
-                    }}
-                />
-            <View style={styles.viewStyle}>
-                <Text style={styles.textStyle}>Home</Text>
-            </View>
-
-            <View>
-                
-            </View>
-
-
-        </View>
-    );
+function HomeScreen() {
+  return (
+    <View>
+      <Image
+        style={styles.imageStyle} source={{
+          uri: 'https://www.secret-scotland.com/datafiles/uploaded/cmsRefImage/popularPlaces/additional/main/main_77_Duthiebandstand.jpg',
+        }}/>
+      <View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>Home</Text>
+      </View>
+    </View>
+  );
 }
 
-export default App;
+export default HomeScreen;
