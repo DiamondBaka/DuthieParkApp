fab4fe79caa811e664680b286e53f53f3e742ffa
DiamondBaka <tomaszmoszczynski18@gmail.com>
2024-02-24 21:47:13 +0000

    app structure

--- App.jsx
+++ App.jsx
@@ -1,15 +1,6 @@
-import {Button, Text, View, StyleSheet} from 'react-native'
-import styles from './style.js'
-function app() {
-    return(
-        <View style={styles.viewStyle}>
-            <View>
-                <Text style={styles.textStyle}>Hello!!!</Text>
-            </View>
-            <View>
-                <Button title='BUTTON!!!' color='#841584'></Button>
-            </View>
-        </View>
-    )
+import HomeScreen from './Screens/HomeScreen'
+
+function App() {
+    return <HomeScreen />;
 }
-export default app;
+export default App;
