3ccc06dab6cec7c5f76aad8f5321f4f709de6bf5
Craig-Leslie <craig.leslie1@outlook.com>
2024-02-25 00:41:24 +0000

    arry stylez

--- style.js
+++ style.js
@@ -3,8 +3,8 @@ const dimensions = Dimensions.get('window');
 const imageHeight = Math.round(dimensions.width * 9 / 16);
 const imageWidth = dimensions.width;
 const styles=StyleSheet.create({
-    textStyle:{
-        fontSize:50,
+    titleStyle:{
+        fontSize:25,
         color:"white",
         zIndex:1
     },
@@ -19,9 +19,56 @@ const styles=StyleSheet.create({
     imageStyle:{
         width: imageWidth,
         height: imageHeight,
-        resizeMode: 'contain',
-        position:'absolute'
-    }
+        resizeMode: 'cover',
+        position:'absolute',
+        flex:1
+    },
+
+    welcomeText:{
+      padding:20,
+      marginTop:130,
+      backgroundColor:"#adcb95",
+      fontSize:25,
+      
+
+    },
+
+    eventSection:{
+      backgroundColor:"#adcb95",
+      width: "100%",
+    marginTop:20,
+
+    },
+
+    eventImage:{
+      borderRadius:20,
+      width: imageWidth * 0.8,
+      height: imageHeight,
+      marginTop:10,
+      marginBottom:10
+    },
+
+
+  eventStyles:{
+    textAlign: "center",
+      },
+
+  trailSection:{
+    marginTop: 20,
+    backgroundColor:"#adcb95",
+    width: "90%",
+    marginLeft:imageWidth * 0.05,
+    flex: 1,
+    justifyContent: "center", 
+    alignItems: "center", 
+    padding: 30,
+    borderRadius: 25,
+  },
+
+  trailText:{
+    fontSize: 30
+
+  }
 
 })
 export default styles;
